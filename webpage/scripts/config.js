4b359e43af3f6cd61ae6adfe6cd252bfb19114d6
LeoMarius <Mariusleo64@gmail.com>
2018-02-28 17:20:53 +0100

    All in one page
    Add config file for all the api keys, make it simpler and cleaner
    All data are loaded directly from adafruit io for all pot.

---
+++ webpage/scripts/config.js
@@ -0,0 +1,43 @@
+const makersPotpots = [
+    {
+        name: "ecu1",
+        token: "a183c8c0ba3f45d2b4a272a10f4b1df1",
+        feeds: "default"
+    }, {
+        name: "ecu2",
+        token: "c507493b545e418b875c2c6969e11e1a",
+        feeds: "default"
+    }, {
+        name: "ecu3",
+        token: "bc95679d06ad46e3bda4de779f8c3b32",
+        feeds: "default"
+    }, {
+        name: "ecu4",
+        token: "5eb59cef3aea40b78b433f86b34f91c7",
+        feeds: "default"
+    }, {
+        name: "ste1",
+        token: "731f8e939f784b3ba8b6229e46dde1b0",
+        feeds: "default"
+    }, {
+        name: "ste2",
+        token: "533668be53204f658e7452974a88463f",
+        feeds: "default"
+    }, {
+        name: "ste3",
+        token: "ddd980a3fd8f4630aa485946b0b3169a",
+        feeds: "default"
+    }, {
+        name: "par1",
+        token: "0debe2dd12964d91841747106f174353",
+        feeds: "default"
+    }, {
+        name: "par2",
+        token: "fb7d00447a644fd8a85bedb580445858",
+        feeds: "default"
+    }, {
+        name: "par3",
+        token: "731d2df62fc34f2291bb9a74ca138c24",
+        feeds: "default"
+    }
+]
---
+++ webpage/scripts/index.js
@@ -0,0 +1,95 @@
+initPotList();
+
+
+function initPotList() {
+
+    var potContent = document.getElementById("potContent");
+
+    potContent.innerHTML = (`<h2 class="titre" >Liste des potagers</h2>
+            <p class="smallText">Cliquez sur un potager pour avoir les informations le concernant</p>
+            <div id="potList" class="squareGrid">
+            </div>`);
+
+    var potList = document.getElementById("potList");
+
+
+    makersPotpots.forEach(function (potpot) {
+        potList.innerHTML = potList.innerHTML + (newPotThumb(potpot.name, potpot.feeds, potpot.token));
+    });
+}
+
+function newPotThumb(name, feeds, token) {
+
+    return (`<div class="potpot ${name}">
+        <img src="medias/potThumb/${name}.jpg" alt="${name} picture" class="image" height="100%" width="100%">
+        <div class="middle" onclick="initPotData('${name}','${feeds}','${token}')">
+            <div class="text">${name}</div>
+        </div>
+    </div>`);
+}
+
+
+
+function initPotData(name, feed, token) {
+
+    let potContent = document.getElementById("potContent");
+
+    ///The group/feed name and the API token (could be smart to parse this on server side)
+    let feedName = name;
+    let apiToken = token;
+    const potpotName = name
+
+    potContent.innerHTML = (`<h2 id="potpotName">Loading datas</h2>
+            <p id="potpotDescription">Si ça attends trop longtemps il y a peu être un problème</p>
+            <div id="dataList" class="squareGrid">
+            </div>`);
+    getDatas(feed, token);
+
+}
+
+function getDatas(feedName, apiToken) {
+    var ioLink = `https://io.adafruit.com/api/groups/${feedName}/receive.json?x-aio-key=${apiToken}`;
+    console.log(ioLink);
+    fetch(ioLink).then(r => r.json())
+        .then(data => computeData(data))
+        .catch(e => console.log("pas ok"))
+}
+
+function computeData(data) {
+    console.log(data);
+
+    var potpotName = document.getElementById("potpotName");
+    var potpotDescription = document.getElementById("potpotDescription");
+
+    potpotName.innerHTML = data.name;
+    potpotDescription.innerHTML = data.description;
+
+    var dataList = document.getElementById("dataList");
+    dataList.innerHTML = ("");
+
+    (data.feeds).forEach(function (element) {
+
+
+
+        if (element.stream != undefined) {
+
+
+            let squareImg = (element.name == "waterlevel") ? "medias/img/water.jpg" : "medias/img/pot01.jpg";
+
+            let date = Date.parse(element.last_value_at);
+            let timeDif = parseInt((Date.now() - date) / 1000);
+
+            let html = `<div class="potpot ${potpotName}">
+                        <img src="${squareImg}" alt="${element.name}" class="image" height="100%" width="100%">
+                        <div class="middle">
+                            <div class="text">${element.name} = ${element.stream.value} </div>
+                        </div>
+                    </div>`;
+
+
+            dataList.innerHTML = dataList.innerHTML + (html.toString());
+        }
+
+    });
+
+}
