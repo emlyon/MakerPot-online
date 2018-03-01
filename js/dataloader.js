5f891a46e5ccdb8855599376b60d71785bf4a823
RADISSON lionel <lionel.radisson@gmail.com>
2018-03-01 11:56:07 +0100

    adding messages

--- js/dataloader.js
+++ js/dataloader.js
@@ -1,41 +1,27 @@
-///The group/feed name and the API token (could be smart to parse this on server side)
-let split = location.pathname.split( '/' );
-const cred = creds[ split[ split.length-1 ].split( '.' )[ 0 ] ];
+addEventListener( 'load', e => {
+    let split = location.pathname.split( '/' );
+    const cred = creds[ split[ split.length-1 ].split( '.' )[ 0 ] ];
 
-// function sendData( data ) {
-//     let dataToSend = Object.entries( data ).map( d => d[ 0 ] + "=" + d[ 1 ] ).join( '&&' );
-//
-//     let link = `https://io.adafruit.com/api/groups/${ username }/send.json?x-aio-key=${ apiToken }&${ dataToSend }`;
-//
-//     fetch( link ).then( r => r.json() )
-//         .then( data => computeData( data ) )
-//         .catch( e => console.log( `error: ${ e }` ) )
-// }
+    function getData() {
+        let link = `https://io.adafruit.com/api/v2/${ cred.username }/feeds?X-AIO-Key=${ cred.key }`;
+        // console.log( link );
 
-function getData() {
-    let link = `https://io.adafruit.com/api/groups/${ cred.username }/receive.json?x-aio-key=${ cred.key }`;
-    // console.log( link );
-
-    fetch( link ).then( r => r.json() )
+        fetch( link ).then( r => r.json() )
         .then( data => computeData( data ) )
         .catch( e => console.log( `error: ${ e }` ) );
-}
+    }
 
-function computeData( data ) {
-    console.log( data );
+    function computeData( data ) {
+        console.log( data );
 
-    document.getElementById( "potpotName" ).innerText = data.name;
-    document.getElementById( "potpotDescription" ).innerHTML = data.description;
+        leds:{
+            on: "YOU ARE MY SUNSHINE",
+            off: "IF YOU LOVE ME LET ME SLEEP"
+        }
 
-    let dataList = document.getElementById( "potpotList" );
-    // dataList.innerHTML = data.feeds.reduce( ( html, element ) => {
-    //     let date = Date.parse( element.last_value_at );
-    //     let timeDif = parseInt((Date.now() - date) / 1000);
-    //     if( element.stream != null ) return html + `<p>${ element.name }: ${ element.stream.value } </p>`;
-    //     else return html;
-    // }, '' );
-}
+    }
 
-///First run of function
-getData();
-setInterval( getData, 60000 );
+    ///First run of function
+    getData();
+    setInterval( getData, 60000 );
+} );
---
+++ js/messages.js
@@ -0,0 +1,17 @@
+const messages = {
+    leds:{
+        lvl0: [ "i feel so fine", "..." ],
+        lvl1: [ "think about me", "..." ],
+        lvl2: [ "please, save me!", "in need of water right now!" ]
+    },
+
+    leds:{
+        on: [ "you are my sunshine", "..." ],
+        off: [ "if you love me let me sleep", "..." ]
+    },
+
+    pump:{
+        on: [ "full power", "..." ],
+        off: [ "taking a break", "..." ]
+    }
+};
