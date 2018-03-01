0815f1a79b635da707dfab10db1e539d754fd3d0
RADISSON lionel <lionel.radisson@gmail.com>
2018-03-01 15:05:14 +0100

    update dataloader

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # makers'pot
 
-For our 5th makers' bootcamp 40 #earlymakers were trained in rapid prototyping and IoT in our makers' lab
-- They made 11 connected and open source indoor gardens in 4 days!
+For our 5th makers' bootcamp, 40 #earlymakers were trained in rapid prototyping and IoT in our makers' labs.  
+They made 11 connected and open source indoor gardens in 4 days!
 
-This repo concerned makers'pots dashbords.
-
-To see makers' pot dashbords :
+This repo concerns makers'pots dashboards accessible here :
 https://sjvl.github.io/MakerPot-online/
 
 To see the video of this event :
 https://youtu.be/-sSJpWdttuQ
 
 
-You can also fin a WIP documentation of makers'pot here http://wikifab.org/wiki/Group:Makers%27_lab
-or here https://www.thingiverse.com/makerslab_emlyon/designs
+You can also find a WIP documentation of makers'pot here: http://wikifab.org/wiki/Group:Makers%27_lab  
+or here: https://www.thingiverse.com/makerslab_emlyon/designs
--- js/dataloader.js
+++ js/dataloader.js
@@ -1,15 +1,35 @@
 addEventListener( 'load', e => {
-    let splitPath = location.pathname.split( '/' );
+    const splitPath = location.pathname.split( '/' );
     const cred = creds[ splitPath[ splitPath.length - 1 ].split( '.' )[ 0 ] ];
+    const link = `https://io.adafruit.com/api/v2/${ cred.username }/feeds?X-AIO-Key=${ cred.key }`;
+    // console.log( link );
 
     ( function getData() {
-        let link = `https://io.adafruit.com/api/v2/${ cred.username }/feeds?X-AIO-Key=${ cred.key }`;
-        // console.log( link );
-
         fetch( link ).then( r => r.json() )
             .then( data => {
                 console.log( data );
 
+                data.forEach( d => {
+                    let survol = null,
+                        value = null;
+                    switch( d.key ){
+                        case 'waterlevel':
+                            survol = document.querySelector( '.water' );
+                            break;
+                        case 'pump':
+                            survol = document.querySelector( '.pump' );
+                            break;
+                        case 'leds':
+                            survol = document.querySelector( '.leds' );
+                            break;
+                    }
+
+                    if( survol != null ){
+                        survol.querySelector( 'span' ).innerText = d.last_value;
+                    }
+                } );
+
+                [].forEach.call( document.querySelectorAll( '.info-text' ), d => d.style.opacity = 1 );
             } )
             .catch( e => console.log( `error: ${ e }` ) );
 
